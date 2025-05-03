a10af7d16bf11cfd3266a3cc9d6b1df522476736
CoreyB <coreyb@noreply.com>
2025-05-04 08:52:31 +1000

    initial

---
+++ lua/commitreminder/init.lua
@@ -0,0 +1,22 @@
+local M = {}
+
+function M.setup(opts)
+	if opts.enabled then
+		local format = "medium"
+		local s = "git log -1 --date=relative --format=" .. format
+		local handle = io.popen(s)
+		require("notify")
+		vim.notify = require("notify")
+		local notifyopts = { title = "Last Commit Reminder", timeout = 5000 }
+		if handle ~= nil then
+			local result = handle:read("*a")
+			handle:close()
+
+			vim.notify(result, vim.log.levels.INFO, notifyopts)
+		else
+			vim.notify("Problem getting git result", vim.log.levels.INFO, notifyopts)
+		end
+	end
+end
+
+return M
